1f9c22082ec0c9a06a75916db07c0c518cf03de1
Mrcheng <1427645611@qq.com>
2020-01-19 17:15:36 +0800

    时间选择器

--- app/src/main/java/com/example/yipartyapp/ClientInfoActivity.java
+++ app/src/main/java/com/example/yipartyapp/ClientInfoActivity.java
@@ -1,40 +1,116 @@
 package com.example.yipartyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.Adapter;
-import android.widget.AdapterView;
-import android.widget.DatePicker;
-import android.widget.Spinner;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Calendar;
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectChangeListener;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
+import com.bigkoo.pickerview.view.TimePickerView;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ClientInfoActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView textView;
+
+    private TimePickerView pvTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_client_info);
+
+
+        textView = findViewById(R.id.bornData);
+        textView.setOnClickListener(this);
+
+        initTimePicker();
+        initCityPicker();
     }
 
-    //出生日期选择
+
+
     @Override
     public void onClick(View view) {
-        Calendar calendar=Calendar.getInstance();
-        new DatePickerDialog( this, new DatePickerDialog.OnDateSetListener() {
+        switch (view.getId()){
+            case R.id.bornData:
+                if (pvTime != null) {
+                    pvTime.show(view);//弹出时间选择器，传递参数过去，回调的时候则可以绑定此view
+                }
+                break;
+        }
+    }
+
+    /**
+     * 出生日期联动选择
+     */
+    private void initTimePicker() {//Dialog 模式下，在底部弹出
+        pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                String text = "你选择了：" + year + "年" + (month + 1) + "月" + dayOfMonth + "日";
-                Toast.makeText( ClientInfoActivity.this, text, Toast.LENGTH_SHORT ).show();
+            public void onTimeSelect(Date date, View v) {
+                textView.setText(getTime(date));
+                Log.i("pvTime", "onTimeSelect");
+
+            }
+        })
+                .setTimeSelectChangeListener(new OnTimeSelectChangeListener() {
+                    @Override
+                    public void onTimeSelectChanged(Date date) {
+                        Log.i("pvTime", "onTimeSelectChanged");
+                    }
+                })
+                .setCancelText("取消")//取消按钮文字
+                .setSubmitText("确定")//确认按钮文字
+                .setType(new boolean[]{true, true, true, false, false, false})
+                .isDialog(true) //默认设置false ，内部实现将DecorView 作为它的父控件。
+                .build();
+
+        Dialog mDialog = pvTime.getDialog();
+        if (mDialog != null) {
+
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    Gravity.BOTTOM);
+
+            params.leftMargin = 0;
+            params.rightMargin = 0;
+            pvTime.getDialogContainerLayout().setLayoutParams(params);
+
+            Window dialogWindow = mDialog.getWindow();
+            if (dialogWindow != null) {
+                dialogWindow.setWindowAnimations(com.bigkoo.pickerview.R.style.picker_view_slide_anim);//修改动画样式
+                dialogWindow.setGravity(Gravity.BOTTOM);//改成Bottom,底部显示
             }
         }
-                ,calendar.get(Calendar.YEAR)
-                ,calendar.get(Calendar.MONTH)
-                ,calendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+    private String getTime(Date date) {//可根据需要自行截取数据显示
+        Log.d("getTime()", "choice date millis: " + date.getTime());
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
+    }
 
+    /**
+     * 出生地址联动选择
+     */
+    private void initCityPicker() {
     }
 }
