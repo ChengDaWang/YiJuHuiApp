b9b20a0382ccb2d5cedbf7594365cbef04fb8c56
Mrcheng <1427645611@qq.com>
2020-01-23 21:25:48 +0800

    城市，学校json数据

--- app/src/main/java/com/example/yipartyapp/ClientInfoActivity.java
+++ app/src/main/java/com/example/yipartyapp/ClientInfoActivity.java
@@ -15,12 +15,16 @@ import android.widget.Toast;
 
 import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
 import com.bigkoo.pickerview.builder.TimePickerBuilder;
-import com.bigkoo.pickerview.listener.OnOptionsSelectChangeListener;
 import com.bigkoo.pickerview.listener.OnTimeSelectChangeListener;
 import com.bigkoo.pickerview.listener.OnTimeSelectListener;
 import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.bigkoo.pickerview.view.TimePickerView;
 import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.example.yipartyapp.Utils.GetJsonDataUtil;
+import com.example.yipartyapp.bean.JsonBean;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -28,9 +32,11 @@ import java.util.Date;
 import java.util.List;
 
 public class ClientInfoActivity extends AppCompatActivity implements View.OnClickListener{
-    private TextView textView;
-
+    private TextView textView;//时间
+    private TextView textView1;//地址
+    private OptionsPickerView pvOptions;
     private TimePickerView pvTime;
+    private static Boolean goFalg=false;//地址选择器加载与否判断标记
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +44,20 @@ public class ClientInfoActivity extends AppCompatActivity implements View.OnClic
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_client_info);
 
-
+        /**
+         * 时间选择
+         */
         textView = findViewById(R.id.bornData);
         textView.setOnClickListener(this);
+        /**
+         * 地址选择
+         */
+        textView1=findViewById(R.id.bornAdress);
+        textView1.setOnClickListener(this);
 
-        initTimePicker();
-        initCityPicker();
+            initTimePicker();
+            initCityPicker();
+
     }
 
 
@@ -51,6 +65,12 @@ public class ClientInfoActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onClick(View view) {
         switch (view.getId()){
+            case R.id.bornAdress:
+
+                if (pvOptions != null) {
+                    pvOptions.show(view);//弹出城市选择器，传递参数过去，回调的时候则可以绑定此view
+                }
+                break;
             case R.id.bornData:
                 if (pvTime != null) {
                     pvTime.show(view);//弹出时间选择器，传递参数过去，回调的时候则可以绑定此view
@@ -58,7 +78,6 @@ public class ClientInfoActivity extends AppCompatActivity implements View.OnClic
                 break;
         }
     }
-
     /**
      * 出生日期联动选择
      */
@@ -111,6 +130,107 @@ public class ClientInfoActivity extends AppCompatActivity implements View.OnClic
     /**
      * 出生地址联动选择
      */
+    private List<JsonBean> options1Items = new ArrayList<>();
+    private ArrayList<ArrayList<String>> options2Items = new ArrayList<>();
+    private ArrayList<ArrayList<ArrayList<String>>> options3Items = new ArrayList<>();
+
+
     private void initCityPicker() {
+            initJsonData();//解析Json数据方法
+            showPickerView();//弹出选择框方法
+    }
+    private void showPickerView() {// 弹出选择器
+            pvOptions = new OptionsPickerBuilder(this, new OnOptionsSelectListener() {
+
+                @Override
+                public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                        //返回的分别是三个级别的选中位置
+                        String opt1tx = options1Items.size() > 0 ?
+                                options1Items.get(options1).getPickerViewText() : "";
+
+                        String opt2tx = options2Items.size() > 0
+                                && options2Items.get(options1).size() > 0 ?
+                                options2Items.get(options1).get(options2) : "";
+
+                        String opt3tx = options2Items.size() > 0
+                                && options3Items.get(options1).size() > 0
+                                && options3Items.get(options1).get(options2).size() > 0 ?
+                                options3Items.get(options1).get(options2).get(options3) : "";
+
+                        String tx = opt1tx + opt2tx + opt3tx;
+                        textView1.setText(tx);//将选择中的数据带回textView中显示
+                    }
+            })
+
+
+                    .setDividerColor(Color.BLACK)
+                    .setTextColorCenter(Color.BLACK) //设置选中项文字颜色
+                    .setContentTextSize(20)
+                    .setCancelText("取消")//取消按钮文字
+                    .setSubmitText("确定")//确认按钮文字
+                    .build();
+        /*pvOptions.setPicker(options1Items);//一级选择器
+        pvOptions.setPicker(options1Items, options2Items);//二级选择器*/
+            pvOptions.setPicker(options1Items, options2Items, options3Items);//三级选择器
+
+        }
+
+
+    private void initJsonData() {//解析数据
+        String JsonData = new GetJsonDataUtil().getJson(this, "province");//获取json文件数据
+        ArrayList<JsonBean> jsonBean = parseData(JsonData);//用Gson 转成实体
+
+        /**
+         * 添加省份数据
+         *
+         * 注意：如果是添加的JavaBean实体，则实体类需要实现 IPickerViewData 接口，
+         * PickerView会通过getPickerViewText方法获取字符串显示出来。
+         */
+        options1Items = jsonBean;
+
+        for (int i = 0; i < jsonBean.size(); i++) {//遍历省份
+            ArrayList<String> cityList = new ArrayList<>();//该省的城市列表（第二级）
+            ArrayList<ArrayList<String>> province_AreaList = new ArrayList<>();//该省的所有地区列表（第三极）
+
+            for (int c = 0; c < jsonBean.get(i).getCityList().size(); c++) {//遍历该省份的所有城市
+                String cityName = jsonBean.get(i).getCityList().get(c).getName();
+                cityList.add(cityName);//添加城市
+                ArrayList<String> city_AreaList = new ArrayList<>();//该城市的所有地区列表
+
+                //如果无地区数据，建议添加空字符串，防止数据为null 导致三个选项长度不匹配造成崩溃
+                /*if (jsonBean.get(i).getCityList().get(c).getArea() == null
+                        || jsonBean.get(i).getCityList().get(c).getArea().size() == 0) {
+                    city_AreaList.add("");
+                } else {
+                    city_AreaList.addAll(jsonBean.get(i).getCityList().get(c).getArea());
+                }*/
+                city_AreaList.addAll(jsonBean.get(i).getCityList().get(c).getArea());
+                province_AreaList.add(city_AreaList);//添加该省所有地区数据
+            }
+
+            /**
+             * 添加城市数据
+             */
+            options2Items.add(cityList);
+
+            /**
+             * 添加地区数据
+             */
+            options3Items.add(province_AreaList);
+        }
+    }
+    private ArrayList<JsonBean> parseData(String result) {//Gson 解析
+        ArrayList<JsonBean> detail = new ArrayList<>();
+        try {
+            JSONArray data = new JSONArray(result);
+            Gson gson = new Gson();
+            for (int i = 0; i < data.length(); i++) {
+                JsonBean entity = gson.fromJson(data.optJSONObject(i).toString(), JsonBean.class);
+                detail.add(entity);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return detail;
     }
 }
