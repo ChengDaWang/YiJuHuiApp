dbb2a7d6a4fd29059a1ef166a470baa4610a0fdf
Mrcheng <1427645611@qq.com>
2020-02-03 22:34:50 +0800

    主页ui布局

--- app/src/main/java/com/example/yipartyapp/MainActivity.java
+++ app/src/main/java/com/example/yipartyapp/MainActivity.java
@@ -1,16 +1,104 @@
 package com.example.yipartyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.yipartyapp.core.MatchingTP;
+import com.example.yipartyapp.core.MessageTP;
+import com.example.yipartyapp.core.SettingTP;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+    private MatchingTP matchingTP;
+    private MessageTP messageTP;
+    private SettingTP settingTP;
+    private Fragment[] fragments;
+    private int lastfragment;//用于记录上个选择的Fragment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initFragment();
     }
 
+    //初始化页面
+    private void initFragment()
+    {
+
+        matchingTP = new MatchingTP();
+        messageTP = new MessageTP();
+        settingTP = new SettingTP();
+        fragments = new Fragment[]{messageTP,matchingTP,settingTP};
+        lastfragment=0;
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainview,messageTP).show(messageTP).commit();
+        bottomNavigationView=(BottomNavigationView)findViewById(R.id.bnv);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(changeFragment);
+    }
+    //判断选择的菜单
+    private BottomNavigationView.OnNavigationItemSelectedListener changeFragment= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId())
+            {
+                case R.id.id1://消息
+                {
+                    if(lastfragment!=0)
+                    {
+                        switchFragment(lastfragment,0);
+                        lastfragment=0;
+
+                    }
+
+                    return true;
+                }
+                case R.id.id2://寻找
+                {
+                    if(lastfragment!=1)
+                    {
+                        switchFragment(lastfragment,1);
+                        lastfragment=1;
+
+                    }
+
+                    return true;
+                }
+                case R.id.id3://设置
+                {
+                    if(lastfragment!=2)
+                    {
+                        switchFragment(lastfragment,2);
+                        lastfragment=2;
+
+                    }
+
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+    };
+    //切换Fragment
+    private void switchFragment(int lastfragment,int index) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.hide(fragments[lastfragment]);//隐藏上个Fragment
+        if (fragments[index].isAdded() == false) {
+            transaction.add(R.id.mainview, fragments[index]);
+
+
+        }
+        transaction.show(fragments[index]).commitAllowingStateLoss();
+    }
 }
---
+++ app/src/main/java/com/example/yipartyapp/core/MatchingTP.java
@@ -0,0 +1,23 @@
+package com.example.yipartyapp.core;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yipartyapp.R;
+
+public class MatchingTP extends Fragment {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.activity_matching_tp,container,false);
+        return view;
+    }
+
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
